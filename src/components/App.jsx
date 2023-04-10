ef7e471435241b352598b4828d5b112b87ecb78a
Maksym Vasiliev <makskremen74@gmail.com>
2023-04-10 21:42:53 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './Friends/FriendList';
 import { TransactionHistory } from './Transactions/Transactions';
 
-import user from './Data/user.json';
-import data from './Data/data.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
+import user from '../Data/user.json';
+import data from '../Data/data.json';
+import friends from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 
 export const App = () => {
   return (
